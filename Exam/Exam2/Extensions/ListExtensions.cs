3ab3d8f2b09c57f941bb4226d009005c4ad41dac
AKruimink <AlwinKruimink@gmail.com>
2020-10-07 20:53:34 +0100

    Add list extension to shuffle a list

---
+++ Exam/Exam2/Extensions/ListExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam2.Extensions
+{
+    /// <summary>
+    /// Defines a class that provides extension methods for <see cref="IList{T}"/>
+    /// </summary>
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Randomly shuffles a list of items
+        /// </summary>
+        /// <typeparam name="T"><see cref="Type"/> of items contained in the list</typeparam>
+        /// <param name="list"><see cref="IList{T}"/> to be shuffled</param>
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int location1 = list.Count;
+            while(location1 > 1)
+            {
+                location1--;
+                int location2 = ThreadSafeRandom.GetThreadSafeRandom.Next(location1 + 1);
+                T valueStorage = list[location2]; // Temporarly store the second location while we override it with location1
+                list[location2] = list[location1];
+                list[location1] = valueStorage;
+            }
+        }
+    }
+}
